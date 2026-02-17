9261e01d4cb28eca7b10f52ec5f7db952de277c0
Flavio Henrique <fhs5895santos@outlook.com>
2026-02-17 19:00:51 -0300

    Ajustando URL de produção

--- front-end/src/api_config.js
+++ front-end/src/api_config.js
@@ -1,3 +1,3 @@
 export const API_URL = process.env.NODE_ENV === 'development' 
     ? "http://localhost:5000" 
-    : "https://SUA-URL-DO-BACKEND.onrender.com";
+    : "https://meu-projeto-backend-saas.onrender.com";
